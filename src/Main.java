f0f496f23d0c874348fa63d1e4f6974ea9501d9a
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2024-01-13 12:58:44 -0600

    commit 1: test1.

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] rps = {"r", "p", "s"};
+		String computerMove = rps[new Random().nextInt(rps.length)];
+		
+		Scanner scanner = new Scanner(System.in);
+		String playerMove;
+		
+		while(true) {
+			System.out.println("Please enter your move (r, p, or s)");
+			playerMove = scanner.nextLine();
+			if (playerMove.equals("r") || playerMove.equals("p") || playerMove.equals("s")) {
+				break;
+			}
+			System.out.println(playerMove + " is not a valid move.");
+		}
+		System.out.println("Computer player: " + computerMove);
+		if (playerMove.equals(computerMove)) {
+			System.out.println("The game was a tie");
+		}
+		else if(playerMove.equals("r") && computerMove.equals("s")){
+			System.out.println("Player wins.");
+		}		
+		else if(playerMove.equals("s") && computerMove.equals("r")){
+			System.out.println("Player loses.");
+		}
+		else if(playerMove.equals("r") && computerMove.equals("p")){
+			System.out.println("Player loses.");
+		}
+		else if(playerMove.equals("p") && computerMove.equals("r")){
+			System.out.println("Player wins.");
+		}
+		else if(playerMove.equals("s") && computerMove.equals("p")){
+			System.out.println("Player wins.");
+		}
+		else if(playerMove.equals("p") && computerMove.equals("s")){
+			System.out.println("Player loses.");
+		}
+	}
+
+}
