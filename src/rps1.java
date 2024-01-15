7c6c968285a341c1f908f48b9325f3f461b39887
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2024-01-15 15:49:19 -0600

    commit 3: redid rps

--- src/rps1.java
+++ src/rps1.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class rps1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -13,37 +13,36 @@ public class Main {
 		String playerMove;
 		
 		while(true) {
-			System.out.println("Please enter your move (r, p, or s)");
+			System.out.println("Rock - r, paper - p or scissor - s?");
 			playerMove = scanner.nextLine();
 			if (playerMove.equals("r") || playerMove.equals("p") || playerMove.equals("s")) {
 				break;
 			}
-			System.out.println(playerMove + " is not a valid move.");
-		}		
+			System.out.println(playerMove + " is not valid");
+		}
 		
 		System.out.println("Computer player: " + computerMove);
 		
 		if (playerMove.equals(computerMove)) {
-			System.out.println("The game was a tie");
+			System.out.println("We have a tie breaker, play again.");
 		}
-		else if(playerMove.equals("r") && computerMove.equals("s")){
+		if (playerMove.equals("r") && computerMove.equals("s")) {
 			System.out.println("Player wins.");
-		}		
-		else if(playerMove.equals("s") && computerMove.equals("r")){
+		}else if(playerMove.equals("s") && computerMove.equals("r")) {
 			System.out.println("Player loses.");
 		}
-		else if(playerMove.equals("r") && computerMove.equals("p")){
+		if (playerMove.equals("r") && computerMove.equals("p")) {
 			System.out.println("Player loses.");
-		}
-		else if(playerMove.equals("p") && computerMove.equals("r")){
+		}else if(playerMove.equals("p") && computerMove.equals("r")) {
 			System.out.println("Player wins.");
 		}
-		else if(playerMove.equals("s") && computerMove.equals("p")){
+		if (playerMove.equals("p") && computerMove.equals("s")) {
+			System.out.println("Player loses.");
+		}else if(playerMove.equals("s") && computerMove.equals("p")) {
 			System.out.println("Player wins.");
 		}
-		else if(playerMove.equals("p") && computerMove.equals("s")){
-			System.out.println("Player loses.");
-		}
+		
+		
 	}
 
 }
