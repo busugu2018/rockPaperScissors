b59482f061d42ca64b0cf1dcabc474d32cacd726
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2024-01-16 09:24:12 -0600

    commit 4: day 4. Repeat.

--- src/rps1.java
+++ src/rps1.java
@@ -6,7 +6,7 @@ public class rps1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String[] rps = {"r", "p", "s"};
+	/*	String[] rps = {"r", "p", "s"};
 		String computerChoice = rps[new Random().nextInt(rps.length)];
 		
 		Scanner scanner = new Scanner(System.in);
@@ -18,27 +18,89 @@ public class rps1 {
 			if(playerChoice.equals("r")|| playerChoice.equals("s") || playerChoice.equals("p")) {
 				break;
 			}
-			System.out.println("invalid entry. Please try again.");
+			System.out.println("Invalid entry. Please try again.");
 		}
+		
 		System.out.println("Computer choice is: " + computerChoice);
+		
 		if(playerChoice.equals(computerChoice)) {
 			System.out.println("Tie. Try again.");
-		}
-		if(playerChoice.equals("r") && playerChoice.equals("s")){
+		}else if(playerChoice.equals("r") && playerChoice.equals("s")){
 			System.out.println("Player wins.");
 		}else if(playerChoice.equals("s") && playerChoice.equals("r")) {
 			System.out.println("Player loses.");
-		}
-		if(playerChoice.equals("r") && playerChoice.equals("p")){
+		}else if(playerChoice.equals("r") && playerChoice.equals("p")){
 			System.out.println("Player loses.");
 		}else if(playerChoice.equals("p") && playerChoice.equals("r")) {
 			System.out.println("Player wins.");
-		}
-		if(playerChoice.equals("p") && playerChoice.equals("s")){
+		}else if(playerChoice.equals("p") && playerChoice.equals("s")){
 			System.out.println("Player loses.");
 		}else if(playerChoice.equals("s") && playerChoice.equals("p")) {
 			System.out.println("Player wins.");
 		}
+		*/
+		
+		
+		String[] rps = {"r", "p", "s"};
+		String computerChoice = rps[new Random().nextInt(rps.length)];
+		
+		Scanner scanner = new Scanner(System.in);
+		String playerChoice;
+		
+		while(true) {
+			System.out.println("Rock, paper or scissors?");
+			playerChoice = scanner.nextLine();
+			if (playerChoice.equals(computerChoice)) {
+				break;
+			}
+			System.out.println("Tie... Play again:");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
